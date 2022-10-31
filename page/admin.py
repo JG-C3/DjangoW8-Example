0a176d6fe311fb10086b4ae44fbc0dde2107b806
JG-C3 <jgc3django@gmail.com>
2022-10-31 13:14:53 +0900

    Update Django W8

--- page/admin.py
+++ page/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-# [코드 추가] models.py의 Comment 모델 추가로 불러오기
 from .models import Posting, Comment
 
 # Register your models here.
 admin.site.register(Posting)
-# [코드 추가] Comment 모델 등록
 admin.site.register(Comment)
