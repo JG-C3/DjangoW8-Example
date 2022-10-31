c4c7956602b3b9e5a4712cb1cadfa64aaf3b3a95
JG-C3 <jgc3django@gmail.com>
2022-10-31 12:46:10 +0900

    Update Django W8

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Weapon #, Character
+from .models import Weapon
 
 # Register your models here.
 admin.site.register(Weapon)
-# admin.site.register(Character)
